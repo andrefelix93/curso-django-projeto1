a6354d8976212d6285b9fd3c306f4e706db5b036
André Felix <dev.andresilva@gmail.com>
2023-05-18 09:37:17 -0300

    Primeiro teste usando pytest.

--- recipes/tests.py
+++ recipes/tests.py
@@ -4,4 +4,5 @@ from django.test import TestCase
 # Create your tests here.
 class RecipeURLsTest(TestCase):
     def test_the_pytest_is_ok(self):
-        ...
+        print('Olá Mundo')
+        assert 1 == 1, 'Um é igual a um'

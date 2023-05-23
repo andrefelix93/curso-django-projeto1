3df036cf2d049963ccd9a93190b9ffe204ee2969
André Felix <dev.andresilva@gmail.com>
2023-05-23 11:02:55 -0300

    Alterações nos campos do formulario

--- authors/views.py
+++ authors/views.py
@@ -1,7 +1,9 @@
+from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
+from django.urls import reverse
 
-from .forms import RegisterForm
+from authors.forms import RegisterForm
 
 
 # Create your views here.
@@ -20,5 +22,14 @@ def register_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(request.POST)
+
+    if form.is_valid():
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
+        messages.success(request, 'Your user is created, please log in.')
+
+        del (request.session['register_form_data'])
+        return redirect(reverse('authors:login'))
 
     return redirect('authors:register')
